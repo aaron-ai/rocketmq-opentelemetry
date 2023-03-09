fbaaf80cdfae0f5504bab8a5b78e4d81786b0c9e
凌楚 <yangkun.ayk@alibaba-inc.com>
2023-03-09 15:14:45 +0800

    Initial commit

---
+++ src/main/java/org/example/ProducerExample.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.SessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.client.java.example.ProducerNormalMessageExample;
+import org.apache.rocketmq.shaded.org.slf4j.Logger;
+import org.apache.rocketmq.shaded.org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Hello world!
+ */
+public class ProducerExample {
+    private static final Logger log = LoggerFactory.getLogger(ProducerNormalMessageExample.class);
+
+    public static void main(String[] args) throws ClientException, IOException {
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+
+        // Credential provider is optional for client configuration.
+        String accessKey = "yourAccessKey";
+        String secretKey = "yourSecretKey";
+        SessionCredentialsProvider sessionCredentialsProvider =
+                new StaticSessionCredentialsProvider(accessKey, secretKey);
+
+        String endpoints = "foobar.com:8080";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+                .setEndpoints(endpoints)
+                .setCredentialProvider(sessionCredentialsProvider)
+                .build();
+        String topic = "yourTopic";
+        // In most case, you don't need to create too many producers, singleton pattern is recommended.
+        final Producer producer = provider.newProducerBuilder()
+                .setClientConfiguration(clientConfiguration)
+                // Set the topic name(s), which is optional but recommended. It makes producer could prefetch the topic
+                // route before message publishing.
+                .setTopics(topic)
+                // May throw {@link ClientException} if the producer is not initialized.
+                .build();
+        // Define your message body.
+        byte[] body = "This is a normal message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
+        String tag = "yourMessageTagA";
+        final Message message = provider.newMessageBuilder()
+                // Set topic for the current message.
+                .setTopic(topic)
+                // Message secondary classifier of message besides topic.
+                .setTag(tag)
+                // Key(s) of the message, another way to mark message besides message id.
+                .setKeys("yourMessageKey-1c151062f96e")
+                .setBody(body)
+                .build();
+        try {
+            final SendReceipt sendReceipt = producer.send(message);
+            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+        } catch (Throwable t) {
+            log.error("Failed to send message", t);
+        }
+        // Close the producer when you don't need it anymore.
+        producer.close();
+    }
+}
